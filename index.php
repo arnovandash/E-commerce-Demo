53b31c34439a85b3a265f39eee186ee8e6a39222
Junius LEKGWARA <julekgwa@e5r2p1.jnb.42.fr>
2016-09-17 09:48:20 +0200

    initial website

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Rush 00</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+<body>
+<header>
+	<nav id="nav">
+		<h1>pizzaville</h1>
+	</nav>
+</header>
+<div id="container">
+	<h1 id="title">Get it while it's hot!</h1>
+	<p id="subtitle">We create custom, personal pizzas with quality ingredients in only five minutes.</p>
+	<p id="order">
+		<button>Order online</button>
+	</p>
+</div>
+</body>
+</html>
